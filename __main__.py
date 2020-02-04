038e00fcd3687b08d925f318a944e8b122cab66a
Donald Lieu <dclieu@ucdavis.edu>
2020-02-04 00:13:17 -0800

    first commit

--- __main__.py
+++ __main__.py
@@ -34,7 +34,7 @@ for i in range(0, 10000):
 update_time_1 = process_time()
 print("Updating 10k records took:  \t\t\t", update_time_1 - update_time_0)
 
-"""
+
 # Measuring Select Performance
 select_time_0 = process_time()
 for i in range(0, 10000):
@@ -42,9 +42,6 @@ for i in range(0, 10000):
 select_time_1 = process_time()
 print("Selecting 10k records took:  \t\t\t", select_time_1 - select_time_0)
 """
-
-
-
 # Measuring Aggregate Performance
 agg_time_0 = process_time()
 for i in range(0, 10000, 100): # 0 , 100, .... 9900 
@@ -52,7 +49,6 @@ for i in range(0, 10000, 100): # 0 , 100, .... 9900
 agg_time_1 = process_time()
 print("Aggregate 10k of 100 record batch took:\t", agg_time_1 - agg_time_0)
 
-"""
 # Measuring Delete Performance
 delete_time_0 = process_time()
 for i in range(0, 10000):
