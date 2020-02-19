c80edc3007311f9f600bb2ea38e5d77ba21e9bb2
jschhie6891 <jschhie@ucdavis.edu>
2020-02-19 10:11:47 -0800

    added some ideas for index.py

--- template/index.py
+++ template/index.py
@@ -5,6 +5,7 @@ A data structure holding indices for various columns of a table.
 Key column should be indexd by default, other columns can be indexed through this object. 
 Indices are usually B-Trees, but other data structures can be used as well.
 """
+
 #TODO: Dealing with non unique key values for the same indexed column
 #Example
 # indexcol = 4, keyVal = 2 for both baseRIDs=2 and 3
