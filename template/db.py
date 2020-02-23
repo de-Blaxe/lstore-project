874942a4cc5c3cb7ac96fa8bd3a133415dc74b93
Donald Lieu <dclieu@ucdavis.edu>
2020-02-22 21:16:16 -0800

    Initial commit

--- template/db.py
+++ template/db.py
@@ -1,9 +1,30 @@
 from template.table import Table
+class BufferPool():
+    def __init__(self):
+        # map rid to associated pages
+        self.pages = dict()
+        # map rid to dirty bit
+        self.isDirty = dict()
+        # map rid to eviction score
+        self.evictionScore = dict()
+        # leastUsedRecord is some rid
+        self.leastUsedRecord = 0
+
+    def get_page(self, rid):
+        if rid not in self.pages:
+            self.replace_pages(rid)
+        return self.pages[rid]
+
+    def replace_pages(self, rid):
+        # assuming eviction policy is LRU
+        
+
 
 class Database():
 
     def __init__(self):
         self.tables = dict() # Index tables by their unique names
+        self.bufferpool = BufferPool()
         pass
 
     def open(self, path):
