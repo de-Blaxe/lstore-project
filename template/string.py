819664b9b8422fce78f71f8e1499200b9ed6be83
jschhie6891 <jschhie@ucdavis.edu>
2020-02-27 13:37:02 -0800

    merge conflict

--- template/string.py
+++
@@ -1,9 +0,0 @@
-pagesetname = "123321/Grades"
-for i, val in enumerate(pagesetname):
-    if val == '/':
-        print(pagesetname[i+1:])
-
-result = pagesetname.split('/')
-print("numbers:", result[0])
-print("table name", result[1])
-
