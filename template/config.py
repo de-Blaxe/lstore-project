9456f27e1881ad08c7f378fee3deb0167e5749e1
jschhie <jschhie@ucdavis.edu>
2020-03-10 17:27:39 -0700

    Update config.py

--- template/config.py
+++ template/config.py
@@ -1,12 +1,10 @@
 # Global Settings for the Database
 
 ## Byte-based
-
 PAGE_SIZE = 4096
 DATA_SIZE = 8
 
 ## Decimal-based
-
 INDIRECTION_COLUMN = 0
 RID_COLUMN = 1
 TIMESTAMP_COLUMN = 2
@@ -15,7 +13,7 @@ BASE_RID_COLUMN = 4
 TPS_COLUMN = 5
 INIT_COLS = 6
 
-BUFFER_SIZE = 10 # # TODO: Changed from 10 to 15, Max number of Page Sets in BufferPool
+BUFFER_SIZE = 10 # Max number of Page Sets in BufferPool
 PAGE_CAPACITY = 512 # Max Records per Page
 PAGE_RANGE_FACTOR = 4 # Max Base Sets per Page Range
 
